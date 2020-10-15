6c022b6426f30df93fb3c0c7b2eac1d41d856021
Micha Gattinger <60971014+Ifolje@users.noreply.github.com>
2020-10-15 13:31:22 +0200

    Update GenossenschaftController.php

--- application/modules/default/controllers/GenossenschaftController.php
+++ application/modules/default/controllers/GenossenschaftController.php
@@ -14,10 +14,10 @@ class GenossenschaftController extends Zend_Controller_Action
         }	
         $mail = new Zend_Mail('UTF-8');
         $mail->setBodyText(strip_tags($content));
-        $mail->setFrom('mail@epelia.com', 'Epelia');
-        $mail->addTo('hoesel@derhoesel.de', 'Epelia');
-        $mail->addTo('mail@epelia.com', 'Epelia');
-        $mail->setSubject('Genossenschaft Anmeldung');
+        $mail->setFrom('mail@fairdirect.org', 'OpenFoodBank');
+        $mail->addTo('hoesel@derhoesel.de', 'OpenFoodBank');
+        $mail->addTo('mail@epelia.com', 'OpenFoodBank');
+        $mail->setSubject('Entwicklungsrat Anmeldung');
         $mail->send();
 
     }
